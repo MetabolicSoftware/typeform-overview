8a6b246f37a59d1f4254d2965270bbc03d9158b1
peteruithoven <peter@metabolic.nl>
2022-11-21 12:02:12 +0100

    Remove nested code workaround

--- src/components/FormField.vue
+++ src/components/FormField.vue
@@ -32,7 +32,7 @@ defineProps({
         </template>
       </div>
       <pre
-        class="mt-2 text-sm overflow-x-auto print:[&_code]:whitespace-pre-wrap"
+        class="mt-2 text-sm overflow-x-auto print:whitespace-pre-wrap"
         v-html="field.logic"
       />
     </div>
